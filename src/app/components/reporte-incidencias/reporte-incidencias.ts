1f76e6bcd7d2e89fa129fc90ccdabb7ee0c5f62d
leonel <lj6730677@gmail.com>
2026-07-05 11:50:04 -0500

    doughnut

--- src/app/components/reporte-incidencias/reporte-incidencias.ts
+++ src/app/components/reporte-incidencias/reporte-incidencias.ts
@@ -18,7 +18,7 @@ export class ReporteIncidencias implements OnInit {
   barChartLegend = true;
   barChartLabels: string[] = [];
   barChartData: ChartDataset[] = [];
-  barChartType: ChartType = 'pie';
+  barChartType: ChartType = 'doughnut';
 
   constructor(private iS: IncidenciasServices, private cdr: ChangeDetectorRef) {}
 
